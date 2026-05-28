d21fe2b56385af4de5fd3196de654e96e67ab8fe
lucindo <lucindovazquez@gmail.com>
2026-05-27 21:46:41 -0300

    Se realizo el desarrollo de agregar una transaccion y que la misma se guarde en supabase

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,6 +1,15 @@
 <script setup>
 import { useAuthStore } from '../stores/auth'
 import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import PrimaryButton from '../components/PrimaryButton.vue'
+import NuevaTransaccion from '../components/NuevaTransaccion.vue'
+
+const mostrarModal = ref(false) //elige si muestra o no el popup de nueva transaccion
+
+const handleNuevaTransaccion = () => {
+  mostrarModal.value = true}
+
 
 const auth = useAuthStore()
 const router = useRouter()
@@ -12,6 +21,8 @@ const handleSignOut = async () => {
   } catch (error) {
   }
 }
+
+
 </script>
 
 
@@ -19,10 +30,20 @@ const handleSignOut = async () => {
   <div class="dashboard-container">
     <h1>Bienvenido al Dashboard</h1>
     <p>Esta es una vista protegida que solo los usuarios autenticados pueden ver.</p>
-  </div>
+  
 
     <button v-if="auth.isAuthenticated" @click="handleSignOut()">Cerrar sesión</button>
     <span v-else>No autenticado</span>
+
+    <PrimaryButton :fullWidth="false" @click="handleNuevaTransaccion">
+    Añadir nueva transacción
+    </PrimaryButton>
+    <NuevaTransaccion
+      :show="mostrarModal"
+      @close="mostrarModal = false"
+    />
+  </div>
+  
 
  
 </template>
